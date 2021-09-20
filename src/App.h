76f39ef84a6dd575d1138d94f47492fdd2c2ad46
javiglesias <javiglesias18@gmail.com>
2021-09-20 21:23:40 +0100

    Code refactor to encapsulate the App code.

---
+++ src/App.h
@@ -0,0 +1,34 @@
+#pragma once
+#ifndef C__APP_HEADER
+#define C__APP_HEADER
+#include "vendor/imgui.h"
+#include "vendor/imgui_impl_glfw.h"
+#include "vendor/imgui_impl_opengl3.h"
+#include "glad/glad.h"
+// GLFW goes second
+#include "GLFW/glfw3.h" 
+
+#include "glm.hpp"
+#include "gtc/matrix_transform.hpp"
+#include "gtc/type_ptr.hpp"
+
+#include "Shader.h"
+#include "Model.h"
+#include "Actor.h"
+
+#include "../dependencies/stb_image/stb_image.h"
+
+#include <queue>
+
+#define _(x) #x
+#define FRAMECAP60 0.016
+#define FRAMECAP30 0.033
+#define CHUNK 1000
+
+class App
+{
+	App() {}
+public:
+	int Run(int args, char** argv, unsigned int mode=0);
+};
+#endif // !C__APP_HEADER
---
+++ src/Utils.h
@@ -0,0 +1,130 @@
+#ifndef C__RENDER
+#define C__RENDER
+// Rendering Namespace
+namespace Render
+{
+	enum BASIC_SHAPES
+	{
+		Cube,
+		Cone,
+		Cylinder,
+		Plane,
+		Sphere,
+		Torus,
+		LightBulb,
+		Monkey
+	};
+	struct Shape_model
+	{
+		BASIC_SHAPES shape;
+		Object::Model model_obj;
+	};
+	enum GUI_COMMANDS
+	{
+		Text,
+		SameLine
+	};
+	struct GUI_command
+	{
+		GUI_COMMANDS command;
+		std::string value;
+
+		GUI_command(GUI_COMMANDS _command, std::string _value)
+		{
+			command = _command;
+			value = _value;
+		}
+	};
+
+	struct model_loaded
+	{
+		Object::Model model_load;
+		Shader shader;
+		glm::mat4 model;
+		glm::mat4 view;
+		glm::mat4 projection;
+		glm::vec3 camera_position;
+		std::string name;
+		bool visible = true;
+
+		model_loaded(Object::Model _model_load, Shader _shader,
+			glm::mat4 _model, glm::mat4 _view, glm::mat4 _projection,
+			glm::vec3 _camera_position, std::string _name = "model_",
+			bool _visible = true)
+		{
+			model_load = _model_load;
+			shader = _shader;
+			model = _model;
+			view = _view;
+			projection = _projection;
+			camera_position = _camera_position;
+			name = _name;
+			visible = _visible;
+		}
+	};
+	float SCALE = 1.f;
+
+	std::queue<GUI_command> gui_commands_q;
+	std::vector<Actor> models_loaded;
+	std::vector<Actor> lights_loaded;
+	std::vector<Actor> custom_models;
+	std::string temp_custom_model;
+
+	//DEBUG GUI
+	/*void DEBUG_LOG(std::string title, std::string value)
+	{
+		gui_commands_q.push(Render::GUI_command(
+			Render::GUI_COMMANDS::Text, title.c_str()));
+		gui_commands_q.push(Render::GUI_command(
+			Render::GUI_COMMANDS::SameLine, ""));
+		gui_commands_q.push(GUI_command(
+			GUI_COMMANDS::Text, value.c_str()));
+	}*/
+	// END DEBUG GUI
+	int screen_width = 800, screen_heigth = 600;
+	float z_near = 0.3f, z_far = 100.f;
+	glm::vec3 camera_forward;
+	float field_of_view;
+	float yaw;
+	float pitch;
+	float last_x_position;
+	float last_y_position;
+	bool first_mouse_interaction;
+	glm::vec4 light_ambient(1.f, 1.f, 1.f, 1.f);
+	glm::vec4 light_diffuse(1.f, 1.f, 1.f, 1.f);
+	glm::vec4 light_specular(1.f, 1.f, 1.f, 1.f);
+	glm::vec3 light_position(0.f);
+	glm::vec3 light_directional(0.f, 0.f, 1.f);
+	float light_k_constant = 1.0;
+	float light_k_linear = 0.09f;
+	float light_k_quadratic = 0.032f;
+	glm::vec3 point_lights_position[4];
+	float shininess = 32.f;
+	float light_cutoff = 12.5f;
+	float light_outer_cutoff = 17.2f;
+	glm::vec3 coral(1.f, .5f, .31f);
+	bool VAO_MODE = true;
+	glm::vec3 camera_position = glm::vec3(0.f, 0.f, 0.3f);
+	glm::vec3 camera_up = glm::vec3(0.f, 1.f, 0.f);
+	glm::mat4 view = glm::lookAt(camera_position,
+		Render::camera_forward, camera_up);
+	glm::mat4 projection = glm::mat4(1.f);
+	float const y_constant = camera_position.y;
+	float camera_speed = 0.05f;
+	float delta_time = 0.f;
+	float new_frame = 0.f;
+	float time_accumulated = 0.f;
+	float time_accumulated_physics = 0.f;
+	float frame_cap = FRAMECAP60;
+	float current_frame = 0.f;
+	unsigned int cubes_spawned = 0;
+	bool demo_mode = false;
+	bool custom_model = false;
+	bool show_GUI_cursor = true;
+	unsigned int squared_world_size = CHUNK;
+	bool mouse_movement = false;
+	bool directional_light_enabled = false;
+	bool point_light_enabled = false;
+	bool spot_light_enabled = false;
+}
+#endif
